099be3a1c09339cf67014c4414e33ce3544d47bf
Wagner Sales <salesawagner@gmail.com>
2018-10-06 19:10:44 -0300

    Add xcode project

---
+++ WasMovie/WasMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WasMovie
+//
+//  Created by Wagner Sales on 06/10/18.
+//  Copyright © 2018 Wagner Sales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ WasMovie/WasMovieTests/WasMovieTests.swift
@@ -0,0 +1,34 @@
+//
+//  WasMovieTests.swift
+//  WasMovieTests
+//
+//  Created by Wagner Sales on 06/10/18.
+//  Copyright © 2018 Wagner Sales. All rights reserved.
+//
+
+import XCTest
+@testable import WasMovie
+
+class WasMovieTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
